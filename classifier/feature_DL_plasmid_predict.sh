1f7ceb3f0fd8b0e611cf5a2aff01eba1b84e2d8a
Bill Andreopoulos <wandreopoulos@lbl.gov>
2018-06-21 17:28:10 -0700

    srv
    Former-commit-id: e14c69a44cb8531019fdb7077e68f826dc40e61d

--- classifier/feature_DL_plasmid_predict.sh
+++ classifier/feature_DL_plasmid_predict.sh
@@ -14,7 +14,7 @@ chmod 777 $OUT
 
 DATETIME=`date "+%Y-%m-%d_%H:%M:%S"`
 
-python2.7 read_fasta2_plasmids.py  -i $FASTA -o $OUT/$DATETIME
+python2.7 /srv/jgi-ml/classifier/read_fasta2_plasmids.py  -i $FASTA -o $OUT/$DATETIME
 
 if [ $? -ne 0 ];
 then echo "read_fasta2_plasmids.py failed"
@@ -29,14 +29,14 @@ mkdir outKF
 mkdir plotKF
 
 #pass fasta and yml directory to format
-python3 format_Test.py  --inputfasta $FASTA --inputyml $OUT/$DATETIME/yml --dataPath $OUT/dlDataPath
+python3 /srv/jgi-ml/classifier/format_Test.py  --inputfasta $FASTA --inputyml $OUT/$DATETIME/yml --dataPath $OUT/dlDataPath
 
 if [ $? -ne 0 ];
 then echo "format_Test.py failed"
 exit 1
 fi
 
-python3 predict_Plasmid.py --dataPath $OUT/dlDataPath  --given test  --kModelList  20-29  --dataSegment -1  --seedModel  models/plasmid4g-
+python3 /srv/jgi-ml/classifier/predict_Plasmid.py --dataPath $OUT/dlDataPath  --given test  --kModelList  20-29  --dataSegment -1  --seedModel  models/plasmid4g-
 
 if [ $? -ne 0 ];
 then echo "predict_Plasmid.py failed"
