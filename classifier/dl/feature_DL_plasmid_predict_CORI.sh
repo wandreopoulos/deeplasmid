e67bb7e1084c8f103f618283e1063695ec9e8113
William B Andreopoulos <andreopo@Williams-MBP.hitronhub.home>
2020-01-13 21:53:03 -0800

    fixed echo sh script description
    Former-commit-id: 2de85ed45cdf4dd63288d82308245f3c480989b4

--- classifier/dl/feature_DL_plasmid_predict_CORI.sh
+++ classifier/dl/feature_DL_plasmid_predict_CORI.sh
@@ -1,20 +1,41 @@
 #!/bin/bash -l
+set -e
 
-echo "Running feature_DL_plasmid_predict.sh . This version is meant for Cori.
-DelPlasmid - Plasmid finder
-Author: Bill Andreopoulos
-Last maintained: July 30, 2019"
+echo "Deeplasmid - Plasmid finder for microbial genome assemblies. 
+Running feature_DL_plasmid_predict_CORI.sh .
+This .sh script is meant for running Deeplasmid native on Cori.
+Usage: please specify 2 arguments - the input fasta file and output directory - as follows:
+     feature_DL_plasmid_predict_CORI.sh  in.fasta outdir
+Contact person: Bill Andreopoulos, wandreopoulos@lbl.gov
+Last maintained: January 12, 2020"
 
 #module load deeplearning
+
+if [ $# -ne 2 ]; then
+    echo "Usage of Deeplasmid on the command line. Please specify 2 arguments (input fasta file and output directory):   feature_DL_plasmid_predict_CORI.sh  in.fasta outdir"
+    exit 1
+fi
+
+trap 'pkill -P $$' EXIT
 
 FASTA=$1
 
 OUT=$2
 
-mkdir $OUT
-chmod 777 $OUT
+mkdir -p $OUT
+if [ $? -ne 0 ];
+then echo "mkdir $OUT failed"
+exit 1
+fi
 
-DATETIME=`date "+%Y-%m-%d_%H:%M:%S"`
+chmod 777 $OUT
+if [ $? -ne 0 ];
+then echo "chmod 777 $OUT failed"
+exit 1
+fi
+
+DATETIME=`date "+%Y%m%d_%H%M%S"`
+echo "Using $DATETIME for outdir suffix"
 
 module unload python
 module load python/2.7-anaconda-4.4
@@ -29,15 +50,11 @@ then echo "read_fasta2_plasmids.py failed"
 exit 1
 fi
 
-mkdir $OUT/dlDataFormattedPred.$DATETIME
+mkdir -p $OUT/dlDataFormattedPred.$DATETIME
 chmod 777 $OUT/dlDataFormattedPred.$DATETIME
 #outPR.$DATETIME stored a pdf file with prediction data and yml files for each sequence, the plots with histograms and the predictions.txt file
-mkdir $OUT/outPR.$DATETIME
+mkdir -p $OUT/outPR.$DATETIME
 chmod 777 $OUT/outPR.$DATETIME
-#These dirs are meant for storing plots, but unused atm.
-#mkdir out
-#mkdir outKF
-#mkdir plotKF
 
 module unload python
 module load python/3.6-anaconda-5.2
@@ -46,24 +63,14 @@ module load python/3.6-anaconda-5.2
 export HDF5_USE_FILE_LOCKING=FALSE
 
 #pass fasta and yml directory to format
-python3 $PARENT/format_predict.py  --inputfasta $FASTA --inputyml $OUT/dlFeatures.$DATETIME/yml --dataPath $OUT/dlDataFormattedPred.$DATETIME  --outPath  $OUT/outPR.$DATETIME  -X
+python3 $PARENT/format_predict.py  --inputfasta $FASTA --inputyml $OUT/dlFeatures.$DATETIME/yml --dataPath $OUT/dlDataFormattedPred.$DATETIME  --outPath  $OUT/outPR.$DATETIME  --no-Xterm
 
 if [ $? -ne 0 ];
 then echo "format_predict.py failed"
 exit 1
 fi
 
-MODEL=/global/dna/shared/data/functests/Assembly/models/plasmid4k-
-MODEL=/global/cscratch1/sd/andreopo/plasmid4t-
-MODEL=/global/cscratch1/sd/andreopo/plasmid4u-
-MODEL=/global/cscratch1/sd/andreopo/plasmid4v-
-MODEL=/global/dna/shared/data/functests/Assembly/models/plasmid4v-
-MODEL=/global/cscratch1/sd/andreopo/plasmid4w-
-MODEL=/global/cscratch1/sd/andreopo/plasmid4x-newfeat2-
-###/global/projectb/sandbox/rqc/andreopo/src/bitbucket/jgi-ml2/classifier/models/plasmid4k-
-MODEL=/global/cscratch1/sd/andreopo/plasmid4x-newfeat3-
-MODEL=/global/cscratch1/sd/andreopo/plasmidml_tests/plasmid4x-newfeat4-
-MODEL=/global/cscratch1/sd/andreopo/plasmidml_tests/plasmid4z-newfeat5-
+#The model used for prediction
 MODEL=/global/dna/shared/data/functests/Assembly/Plasmid_Models/plasmid4z-newfeat5-
 echo "Model used for prediction: $MODEL" 
 echo "Model used for prediction: $MODEL" > $OUT/outPR.$DATETIME/model_path.txt
@@ -71,12 +78,7 @@ echo "Command used: $0 $1 $2"
 echo "Command used: $0 $1 $2" >> $OUT/outPR.$DATETIME/model_path.txt
 
 
-python3 $PARENT/predict_Plasmid.py --dataPath $OUT/dlDataFormattedPred.$DATETIME   --outPath  $OUT/outPR.$DATETIME   --dataSegment -1   --given test  --kModelList  18-29  --seedModel  $MODEL 
-# /global/projectb/sandbox/rqc/andreopo/src/bitbucket/jgi-ml2/classifier/models/plasmid4k-
-### /global/cscratch1/sd/andreopo/plasmid4p-
-### /global/projectb/sandbox/rqc/andreopo/src/bitbucket/jgi-ml_rqcprod/classifier/models/plasmid4k-
-### /global/cscratch1/sd/andreopo/plasmid4o-  
-###$PARENT/models/plasmid4k-
+python3 $PARENT/predict_Plasmid.py --dataPath $OUT/dlDataFormattedPred.$DATETIME   --outPath  $OUT/outPR.$DATETIME   --dataSegment -1   --given test  --kModelList  18-29  --seedModel  $MODEL  --no-Xterm
 
 if [ $? -ne 0 ];
 then echo "predict_Plasmid.py failed"
