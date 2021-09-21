38850ca39032fcb605ffff76a98d21d95fb971d8
lexomobi <lexomobi@lexomobi.home>
2021-09-20 23:15:59 -0700

    sh train file

--- classifier/dl/feature_DL_plasmid_train_CORI.sh
+++ classifier/dl/feature_DL_plasmid_train_CORI.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 
 echo "Running feature_DL_plasmid_train.sh . This version is meant for Cori.
-DelPlasmid - Plasmid finder
-Author: Bill Andreopoulos
-Last maintained: July 30, 2019"
+Deeplasmid - Plasmid finder
+Author: Bill Andreopoulos, wandreopoulos@lbl.gov
+Last maintained: September 19, 2021"
 
 
 
@@ -21,12 +21,12 @@ OUT=$2
 mkdir $OUT
 chmod 777 $OUT
 
-DATETIME=`date "+%Y-%m-%d_%H:%M:%S"`
+export DATETIME=`date "+%Y-%m-%d_%H:%M:%S"`
 
 module unload python
 module load python/2.7-anaconda-4.4
 
-PARENT=`dirname $0`
+export PARENT=`dirname $0`
 
 
 python2.7 $PARENT/read_fasta2_plasmids.py  -i $FASTA -o $OUT/dlFeatures.$DATETIME
